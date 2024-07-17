1ae3e205ddd0eb1da48e4a9237477afa27f607f4
angu89 <leo898989qaz@gmail.com>
2024-07-17 15:55:40 +0800

    113-07-17

--- 113-07-17/02-Karel/karel_06.py
+++ 113-07-17/02-Karel/karel_06.py
@@ -1,8 +1,19 @@
 from stanfordkarel import *
 import os
 
+def put_5_beepers():
+    for i in range(5):
+        put_beeper()
+
 def main():
     """ Karel code goes here! """
+    for i in range(4):
+        put_5_beepers()
+        move()
+        move()
+        move()
+        turn_left()
+
     pass
 
 if __name__ == "__main__":
